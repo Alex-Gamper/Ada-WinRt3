9f56b0583e9704433e3a0c9b0c776d86fc1a1aa9
Alex Gamper <alex.gamper@masonstevens.com.au>
2026-01-15 16:09:14 +1100

    Fix

--- Build.bat
+++ Build.bat
@@ -2,7 +2,7 @@ cd WinRt3
 cd x64\debug\build
 del *.std*
 del *.obj
-for %%i in (*.o) do convertDwarf %%i
+for %%i in (*.o) do convertDwarf %%~ni
 del WinRt.Windows.UI.Xaml.Controls.obj
 copy WinRt.Windows.UI.Xaml.Controls.o WinRt.Windows.UI.Xaml.Controls.obj
 cd ..
